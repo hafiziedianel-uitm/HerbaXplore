48fe53325b33a8a57a29cf65deadc56a3eb28628
hafiziedianel-uitm <hafiziedianel@uitm.edu.my>
2026-04-05 20:04:05 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css'; // Global styles
 import { ThemeProvider } from '@/components/ThemeProvider';
 
 export const metadata: Metadata = {
-  title: 'HerbalXplore: Farmasi UiTM',
+  title: 'HerbaXplore: Farmasi UiTM',
   description: 'Interactive educational tool for exploring Malaysian medicinal plants, their active compounds, 3D molecular structures, and pharmacological properties.',
 };
 
